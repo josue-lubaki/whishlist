ab1b49d805a84686c7b755ef9de5692e45a0e63b
Josue Lubaki <josuelubaki@gmail.com>
2025-08-16 14:21:55 -0400

    Initial Commit

---
+++ Whishlist/WhishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhishlistApp.swift
+//  Whishlist
+//
+//  Created by Josue Lubaki on 2025-08-16.
+//
+
+import SwiftUI
+
+@main
+struct WhishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
